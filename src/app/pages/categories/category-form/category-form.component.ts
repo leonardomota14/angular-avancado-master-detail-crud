36c2025be4b5f9fbea7d7d8961933c5b0d12d1bf
leonardomota14 <leonardomota14@gmail.com>
2022-03-03 18:39:46 -0300

    imporving category form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import { Router, ActivatedRoute} from '@angular/router';
+
+import { CategoryModel } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
+
+import { switchMap } from 'rxjs/operators';
+import toastr from 'toastr';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: CategoryModel;
+
+  constructor(
+    private service: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryform();
+    this.loadCategory();
+  }
+
+  ngAfterContentChecked() { 
+    this.setPageTitle();  
+  }
+
+  private setCurrentAction() {
+    if(this.route.snapshot.url[0].path == 'new')
+      this.currentAction = 'new';
+    else 
+      this.currentAction = 'edit';
+  }
+
+  private buildCategoryform() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, Validators.required, Validators.minLength(2)],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if(this.currentAction == 'edit') 
+    this.route.paramMap.pipe(
+      switchMap(params => this.service.getById(+params.get('id')))
+    ).subscribe(
+      (category) => {
+        this.category = category;
+        this.categoryForm.patchValue(category); //carregando as categorias para o formGroup
+      },
+      (error) => alert('Ocorreu um erro ao carregar as categorias!')
+    );    
+  }
+
+  private setPageTitle() {
+    if(this.currentAction == 'new') 
+      this.pageTitle = 'Cadastro de Nova Categoria'
+    else {
+      const categoryName = this.category.name || ' ';
+      this.pageTitle = `Editando categoria: ${categoryName}`;
+    }
   }
 
 }
