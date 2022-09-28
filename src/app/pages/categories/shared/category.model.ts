6c7e9c0a8aae85fcfad2961ff0a5cc8d43eeaae8
leonardomota14 <leonardomota14@gmail.com>
2022-09-28 15:17:02 -0300

    Adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,5 +1,12 @@
-export interface CategoryModel {
-  id?:number,
-  name: string,
-  description?: string
+import { BaseResourceModel } from "src/app/shared/models/base-resource.model";
+
+export class CategoryModel extends BaseResourceModel {
+
+  constructor(
+    public id?: number,
+    public name?: string,
+    public description?: string,
+  ) {
+    super();
+  }
 }
